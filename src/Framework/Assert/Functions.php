80d295c8136bd4494756ae3eb62dae58037a116f
Marco De Felice <marco.defelice@qwentes.it>
2023-10-16 15:26:51 +0200

    added new magic function

--- src/Framework/Assert/Functions.php
+++ src/Framework/Assert/Functions.php
@@ -2719,3 +2719,54 @@ if (!function_exists('PHPUnit\Framework\onConsecutiveCalls')) {
         return new ConsecutiveCallsStub($arguments);
     }
 }
+
+if (!function_exists('data_get')) {
+    /**
+     * Get an item from an array or object using "dot" notation.
+     *
+     * @param  mixed  $target
+     * @param  string|array|int|null  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function data_get($target, $key, $default = null)
+    {
+        if (is_null($key)) {
+            return $target;
+        }
+
+        $key = is_array($key) ? $key : explode('.', $key);
+
+        foreach ($key as $segment) {
+            if (is_null($segment)) {
+                return $target;
+            }
+
+            if ($segment === '*') {
+                if (is_a($target, 'Collection')) {
+                    $target = $target->all();
+                } elseif (!is_iterable($target)) {
+                    return $default;
+                }
+
+                $result = [];
+
+                foreach ($target as $item) {
+                    $result[] = data_get($item, $key);
+                }
+
+                return in_array('*', $key) ? array_merge(...$result) : $result;
+            }
+
+            if (is_array($target) && array_key_exists($segment, $target)) {
+                $target = $target[$segment];
+            } elseif (is_object($target) && property_exists($target, $segment)) {
+                $target = $target->{$segment};
+            } else {
+                return $default;
+            }
+        }
+
+        return $target;
+    }
+}
